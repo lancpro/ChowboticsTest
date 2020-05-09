ddc15dc2d91f88a8fbdc9b1729a614768646b4a0
Ashish <Ashish.Rathi@KLINGELNBERG.com>
2020-05-09 23:18:38 +0530

    Added SDK project along with User model

---
+++ src/UserManagement.SDK/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagement.SDK
+{
+    public class User
+    {
+        public string UserName { get; set; } //Validate minimum length
+        public string Password { get; set; } // Validate password length/no. char
+        public string Email { get; set; } // Validate email address using regex
+    }
+}
--- src/UserManagement/Program.cs
+++ src/UserManagement/Program.cs
@@ -6,8 +6,34 @@ namespace UserManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProvideChoices();
             Console.ReadKey();
+        }
+
+        static void ProvideChoices()
+        {
+            Console.WriteLine("Please select any one option:");
+            Console.WriteLine("1. Login");
+            Console.WriteLine("2. Register");
+            Console.WriteLine("3. Exit");
+            var userChoice = Console.ReadLine();
+            ValidateChoice(userChoice);
+        }
+
+        static void ValidateChoice(string userChoice)
+        {
+            switch (userChoice)
+            {
+                case "1":
+                    var login = new Login();
+                    break;
+                case "2":
+                    var register = new Register();
+                    break;
+                default:
+                    ProvideChoices();
+                    break;
+            }
         }
     }
 }
